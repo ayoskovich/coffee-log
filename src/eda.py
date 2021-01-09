58c6ac7a6ffd7024f2cd51ee9b04455494581360
Anthony Yoskovich <yoskovia@mail.gvsu.edu>
2021-01-09 10:57:49 -0500

    Adding data dictionary'

--- src/eda.py
+++ src/eda.py
@@ -9,3 +9,21 @@ import pandas as pd
 df = pd.read_csv('data/coffee.csv')
 df
 
+
+# In[ ]:
+
+
+[print(x) for x in df.columns]
+
+
+# In[ ]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+df['coffee'].unique()
+
